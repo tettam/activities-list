abcf9cdb38fa7efba0a78e46123d67ad54a38566
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-10-27 20:14:05 -0300

    create migration categories

---
+++ database/migrations/2023_10_27_220331_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('color')->default('#FFFFFF');
+            $table->foreignIdFor(User::class)->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function(Blueprint $table){
+            $table->dropForeign(User::class);
+        });
+        Schema::dropIfExists('categories');
+    }
+}
